edf10d9f76fb4431c73ba08274a34da4ab527a89
aaronzhualt-max <aaronzhualt@gmail.com>
2026-09-16 21:03:06 -0700

    Add files via upload

---
+++ Convert.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Convert {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter a temperature in celsius: ");
+		double celsius = in.nextDouble();
+		final double fahrenheit = celsius*9/5+32;
+		System.out.printf("%.1f C = %.1f F%n", celsius, fahrenheit);
+	}
+}	
+	
